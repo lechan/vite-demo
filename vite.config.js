cd80138761244be395d5aefc6bb29bba135b7bd3
lingchen <lingchen@migu.cn>
2021-08-16 10:46:33 +0800

    fix: format files

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,23 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://192.168.1.1:5000'
+    base: process.env.NODE_ENV === 'production' ? '/' : '/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     },
-    cors: true
-  },
-  build: {
-    outDir: './dist',
-    emptyOutDir: true
-  }
+    server: {
+        port: 3000,
+        open: true,
+        proxy: {
+            '/api': 'http://192.168.1.1:5000'
+        },
+        cors: true
+    },
+    build: {
+        outDir: './dist',
+        emptyOutDir: true
+    }
 })
